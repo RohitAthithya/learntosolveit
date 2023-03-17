e1ac8b6d6cdb010cb74de09560a39e74ad0cee18
RohitAthithya <82933996+RohitAthithya@users.noreply.github.com>
2023-03-17 07:31:58 +0530

    Added another solution
    this solution seems to be of simpler logic : also talks about 'not over engineering' just compensate for loss in logic, which is a common practice among developers!

--- languages/cprogs/Ex_1.20_detab.c
+++ languages/cprogs/Ex_1.20_detab.c
@@ -45,3 +45,59 @@ int main(void)
 
 	return 0;
 }
+
+/* //another solution
+
+say for 4 tabstops:
+tabstops look like this:
+____|____|____|    (they stop at every 4 instances!)
+
+_ represents where the input needs to go!
+
+for 8 tabstops:
+tabstops look like this:
+________|________|________|    (they stop at every 8 instances!)
+
+
+*/
+/*
+#include <stdio.h>
+
+#define TABSTOPLEN 4
+
+main()
+{
+    int chr;
+    int track_length = 0; // to track the length of input
+    int curr_pos;
+    while ((chr = getchar()) != EOF)
+    {
+        track_length++;
+        // need to handle only tabspaces and newlines -- otherwise just print the input character
+        if (chr == '\n')
+        {
+            // print the next line and reset the track_length
+            putchar(chr);
+            track_length = 0;
+        }
+        else if (chr == '\t')
+        {
+            // check the current location - where is it from the previous tabstop
+            curr_pos = (track_length % TABSTOPLEN);
+            while (curr_pos != 0) //
+            {
+                putchar(' ');
+                track_length++;
+            }
+            // extra space is to be put to compensate the loss in logic
+            putchar(' ');
+        }
+        else
+        {
+            putchar(chr);
+        }
+    }
+}
+
+
+*/
